c8e36bafff9b57b14969e9a1a181d982362bf335
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-09 15:22:38 -0600

    Fixed repeat snytax issue.

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -28,7 +28,7 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public User findByUsername(String username) {
-        String query = "SELECT * FROM users WHERE username LIKE ? LIKE 1";
+        String query = "SELECT * FROM users WHERE username LIKE ?";
         try {
             PreparedStatement stmt = connection.prepareStatement(query);
             stmt.setString(1, "%"+username+"%");
