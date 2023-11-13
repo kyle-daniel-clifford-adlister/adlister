dda29cb2dd653aef13e718c93ae3a61f8642b612
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-11-13 12:02:40 -0600

    Added html into the profile jsp for form so user can update their registration/account details (username/email/password). Then created a new UpdateLoginServlet which is a doPost sup up to reassign new user values for their account info.

---
+++ src/main/java/com/codeup/adlister/controllers/UpdateLoginServlet.java
@@ -0,0 +1,62 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
+import org.mindrot.jbcrypt.BCrypt;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name = "controllers.UpdateLoginServlet", urlPatterns = "/updateuser")
+public class UpdateLoginServlet extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String passwordConfirmation = request.getParameter("confirm_password");
+        HttpSession session = request.getSession();
+
+        // validate input
+        boolean inputHasErrors = username.isEmpty()
+                || email.isEmpty()
+                || password.isEmpty()
+                || (! password.equals(passwordConfirmation));
+
+        if (inputHasErrors) {
+            session.setAttribute("error",inputHasErrors);
+            response.sendRedirect("/register");
+            return;
+        }
+        User checkingUser = DaoFactory.getUsersDao().findByUsername(username);
+        if(checkingUser == null) {
+            // create and save a new user
+            User user = new User(username, email, password);
+
+            // hash the password
+
+            String hash = Password.hash(user.getPassword());
+
+            user.setPassword(hash);
+
+            DaoFactory.getUsersDao().insert(user);
+            response.sendRedirect("/profile");
+        }
+        else {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            System.out.println("You will see this in the console");
+            response.getWriter().println("You should see this in the browser");
+            out.println("<script>alert("+"Hello, World!"+")</script>");
+            response.sendRedirect("/profile");
+        }
+    }
+}
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -1,4 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<link rel="stylesheet" href="../css/profilePage.css">
+
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp">
@@ -8,9 +11,29 @@
 <body>
     <jsp:include page="/WEB-INF/partials/navbar.jsp" />
 
-    <div class="container">
+    <div class="container text-center">
         <h1>Welcome, ${sessionScope.user.username}!</h1>
     </div>
+
+    <form id="newUserForm" action="/updateuser" method="POST">
+        <div class="tab">
+            <h1>Update Login Info: </h1>
+            <label id="usernameLabel">Username</label>
+            <p><input id="username" class="username" type="text" name="username"></p>
+            <label id="newEmailLabel">Email</label>
+            <p><input id="email" class="email" type="email" name="email"></p>
+            <label id="newPasswordLabel">Password</label>
+            <p><input id="password" class="password" type="password" name="password"></p>
+            <label id="confirmPasswordLabel">Password</label>
+            <p><input id="confirm_password" class="password" type="password" name="confirm_password"></p>
+        </div>
+
+        <div style="overflow:auto;">
+            <div style="float:left;">
+                <button type="submit" id="submit" >Submit</button>
+            </div>
+        </div>
+    </form>
 
 </body>
 </html>
