eb62288fd3a44794a48bdf6a661cd26bd01e38b8
Clifford Gardner <cliffordgardner9@gmail.com>
2023-11-14 13:46:49 -0600

    Sort by Category Works

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -33,7 +33,7 @@ public class CreateAdServlet extends HttpServlet {
 //        TODO convert to String if doesn't work
         ArrayList<Integer> categoryNames = new ArrayList<>();
 
-        String[] categories = request.getParameterValues("categories");
+        String[] categories = request.getParameterValues("categories[]");
         for (String cateory:categories) {
             int value = Integer.parseInt(cateory);
             System.out.println(value);
